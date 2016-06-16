d4970c4816eccb89c6ee15d8aea3babde77cb2d3
Jonathan Marshall <jmarshall@never.you.mind>
2016-06-17 10:42:36 +1200

    adds updates to forward ports and setup mysql

--- Vagrantfile
+++ Vagrantfile
@@ -2,9 +2,10 @@
 Vagrant.configure(2) do |config|
 
 #  For Mac Dev use the Parallels version of Trusty64 otherwise use the ubuntu box.
-#  config.vm.box = "ubuntu/trusty64"
-  config.vm.box = "parallels/ubuntu-14.04"
-  config.vm.network "forwarded_port", guest: 3838, host: 3838
+  config.vm.box = "ubuntu/trusty64"
+#  config.vm.box = "parallels/ubuntu-14.04"
+  config.vm.network "forwarded_port", guest: 3838, host: 8080
+  config.vm.network "forwarded_port", guest: 3306, host: 3306
 
   config.vm.provider "virtualbox" do |vb|
     vb.customize ["modifyvm", :id, "--memory", "2048"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 # This script will update and install Shiny
 
-sudo apt-get update
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password dbpass'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password dbpass'
 sudo apt-get -y install git gcc
-sudo apt-get -y install libxml2-dev libcurl4-openssl-dev libssl0.9.8 libcairo2-dev
+sudo apt-get -y install libxml2-dev libcurl4-openssl-dev libssl0.9.8 libcairo2-dev libv8-dev
+sudo apt-get install -y mysql-server libmysqlclient-dev
 sudo add-apt-repository ppa:marutter/rrutter
 sudo apt-get update
 sudo apt-get -y install r-base r-base-dev
 sudo R -e "install.packages('shiny', repos = 'http://cran.rstudio.com/', dep = TRUE)"
 sudo R -e "install.packages('rmarkdown', repos = 'http://cran.rstudio.com/', dep = TRUE)"
-sudo R -e "install.packages('RColorBrewer', repos = 'http://cran.rstudio.com/', dep = TRUE)"
+sudo R -e "install.packages('shinyjs', repos = 'http://cran.rstudio.com/', dep = TRUE)"
+sudo R -e "install.packages('RMySQL', repos = 'http://cran.rstudio.com/', dep = TRUE)"
 sudo apt-get -y install gdebi-core
-wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb
-shiny-server-1.3.0.403-amd64.deb
+wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.0.718-amd64.deb
+sudo gdebi shiny-server-1.4.0.718-amd64.deb
+
 sudo dpkg -i *.deb
 rm *.deb
 
 #  Set the applicaiton directory to point to the vagrant share.
-sudo ln -s /vagrant/apps /srv/shiny-server
-sudo ln -s /vagrant/log/shiny-server.log /var/log/shiny-server.log
+sudo ln -sf /vagrant/apps /srv/shiny-server
+sudo ln -sf /vagrant/log/shiny-server.log /var/log/shiny-server.log
