58b601cafea957e5e3ec2d9906fe26ee078300a2
Jonathan Marshall <jmarshall@never.you.mind>
2018-04-11 10:46:09 +1200

    add lubridate/visreg and libxt

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password dbpass'
 sudo apt-get -y install git gcc
 sudo apt-get -y install libxml2-dev libcurl4-openssl-dev libssl0.9.8 libcairo2-dev libv8-dev
+sudo apt-get -y install libxt-dev
 sudo apt-get install -y mysql-server libmysqlclient-dev
 sudo add-apt-repository ppa:marutter/rrutter
 sudo apt-get update
@@ -13,6 +14,8 @@ sudo R -e "install.packages('shiny', repos = 'http://cran.rstudio.com/', dep = T
 sudo R -e "install.packages('rmarkdown', repos = 'http://cran.rstudio.com/', dep = TRUE)"
 sudo R -e "install.packages('shinyjs', repos = 'http://cran.rstudio.com/', dep = TRUE)"
 sudo R -e "install.packages('RMySQL', repos = 'http://cran.rstudio.com/', dep = TRUE)"
+sudo R -e "install.packages('lubridate', repos = 'http://cran.rstudio.com/', dep = TRUE)"
+sudo R -e "install.packages('visreg', repos = 'http://cran.rstudio.com/', dep = TRUE)"
 sudo apt-get -y install gdebi-core
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.0.718-amd64.deb
 sudo gdebi shiny-server-1.4.0.718-amd64.deb
